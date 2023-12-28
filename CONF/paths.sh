625060dabfa8cebf18ef8774e0824df8d9667bcd
Michael Iyke <ymichaelc@gmail.com>
2023-12-28 10:54:56 +0100

    The Betty tar

--- CONF/paths.sh
+++ CONF/paths.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+bin=${bin:-"$HOME/bin"}
 # LOAD CUSTOM PATHS
 
 # DROP A PATH IN BELOW ARRAY
@@ -15,3 +16,13 @@ cust_bin_paths=(
 # WILL BE LOADED BY load_paths FUNCTION WHICH WILL BE EXECUTED
 # FOR EVERY COMMAND TO MAKE NEW PATHS REFLECT QUICKLY
 
+# # Below -s ensures single : each
+cust_bin_paths=$(IFS=:; echo "${cust_bin_paths[*]}")
+
+# Only use the first element of cut_bin_paths - sorta like *cust_bin_paths in C
+# Use that for now till we know why the cust_bin_paths could not be completely
+# overwritten as viewed from ${cust_bin_paths[@]} or ${cust_bin_paths[*]}
+# It still includes some elements of the old array values
+# echo "$cust_bin_paths"
+
+export PATH="$PATH:$cust_bin_paths"
